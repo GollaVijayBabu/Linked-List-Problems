4965717d0795f0e730342cec251ece6fa8b8b9fd
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-11-07 01:59:19 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linked_List_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
