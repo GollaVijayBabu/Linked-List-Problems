a64e50082e0a16d14ac4c859e5b788a214485b18
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-11-07 02:03:11 -0800

    Added Linked List Problems

--- Program.cs
+++ Program.cs
@@ -1,12 +1,137 @@
 ﻿using System;
 
-namespace Linked_List_Problems
+namespace Day_14_Problems
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Weclcome to Day 14 Problems \n");
+            Console.WriteLine("Please select following options \n");
+            Console.WriteLine(" 1.UC_1 \n 2.UC_2 \n 3.UC_3 \n 4.UC_4 \n 5.UC_5 \n " +
+                "6.UC_6 \n 17.UC_7 \n 8.UC_8 \n 9.UC_9 \n 10.UC_10 \n");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    LinkedListOperations<int> linkedListAdd = new LinkedListOperations<int>();
+                    linkedListAdd.add(56);
+                    linkedListAdd.add(30);
+                    linkedListAdd.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListAdd.display();
+                    break;
+
+                case 2:
+                    LinkedListOperations<int> linkedListsequence = new LinkedListOperations<int>();
+                    linkedListsequence.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListsequence.display();
+                    linkedListsequence.InsertAtFirst(30);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListsequence.display();
+                    linkedListsequence.InsertAtFirst(56);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListsequence.display();
+                    break;
+
+                case 3:
+                    LinkedListOperations<int> linkedListseq = new LinkedListOperations<int>();
+                    linkedListseq.add(56);
+                    linkedListseq.add(30);
+                    linkedListseq.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListseq.display();
+                    break;
+
+                case 4:
+                    LinkedListOperations<int> linkedListInsert = new LinkedListOperations<int>();
+                    linkedListInsert.add(56);
+                    linkedListInsert.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListInsert.display();
+                    linkedListInsert.InsertAfter(56, 30);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListInsert.display();
+
+                    break;
+
+                case 5:
+                    LinkedListOperations<int> linkedListDelete = new LinkedListOperations<int>();
+                    linkedListDelete.add(56);
+                    linkedListDelete.add(30);
+                    linkedListDelete.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListDelete.display();
+                    linkedListDelete.RemoveFirst();
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListDelete.display();
+                    break;
+
+                case 6:
+                    LinkedListOperations<int> linkedListDel = new LinkedListOperations<int>();
+                    linkedListDel.add(56);
+                    linkedListDel.add(30);
+                    linkedListDel.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListDel.display();
+                    linkedListDel.RemoveLast();
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListDel.display();
+                    break;
+
+                case 7:
+                    LinkedListOperations<int> linkedListSearch = new LinkedListOperations<int>();
+                    linkedListSearch.add(56);
+                    linkedListSearch.add(30);
+                    linkedListSearch.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    linkedListSearch.display();
+                    linkedListSearch.search(30);
+                    break;
+
+                case 8:
+                    LinkedListOperations<int> Insert = new LinkedListOperations<int>();
+                    Insert.add(56);
+                    Insert.add(30);
+                    Insert.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    Insert.display();
+                    Insert.InsertAfter(30, 40);
+                    Console.WriteLine("Linked list sequence is");
+                    Insert.display();
+                    break;
+
+                case 9:
+                    LinkedListOperations<int> Delete = new LinkedListOperations<int>();
+                    Delete.add(56);
+                    Delete.add(30);
+                    Delete.add(70);
+                    Console.WriteLine("Linked list sequence is");
+                    Delete.InsertAfter(30, 40);
+                    Console.WriteLine("Linked list sequence is");
+                    Delete.display();
+                    Delete.Remove(40);
+                    Console.WriteLine("Linked list sequence is");
+                    Delete.display();
+                    break;
+
+                case 10:
+                    LinkedListOperations<int> Sort = new LinkedListOperations<int>();
+                    Sort.addSort(56);
+                    Sort.addSort(30);
+                    Sort.addSort(40);
+                    Sort.addSort(70);
+                    Console.WriteLine("Linked list sequence is");
+                    Sort.display();
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+
         }
     }
 }
---
+++ linked list problemss.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace Day_14_Problems
+{
+    public class LinkedListOperations<T>
+    {
+        public Node<T> head;
+
+        public void add(T data)
+        {
+            Node<T> node = getNode(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node<T> temp = head;
+
+                while (temp.next != null)
+                    temp = temp.next;
+
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into link list", node.Data);
+        }
+
+        public void addSort(T data)
+        {
+            Node<T> node = getNode(data);
+
+            if (head == null || head.Data >= node.Data)
+            {
+                node.next = head;
+                head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+
+                while (temp.next != null && temp.next.Data < node.Data)
+                {
+                    temp = temp.next;
+                }
+                node.next = temp.next;
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into link list", node.Data);
+        }
+
+        private Node<T> getNode(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.next = null;
+            return newNode;
+        }
+
+        public void display()
+        {
+            if (head == null)
+                Console.WriteLine("List is empty");
+            else
+            {
+                Node<T> temp = head;
+                while (temp != null && temp.Data != null)
+                {
+                    Console.WriteLine(temp.Data + "\t");
+                    temp = temp.next;
+                }
+            }
+        }
+
+        public void InsertAtFirst(T data)
+        {
+            Node<T> node = getNode(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                Node<T> temp = head;
+                head = node;
+                head.next = temp;
+            }
+            Console.WriteLine("{0} inserted into link list", node.Data);
+        }
+
+        public void InsertAfter(T specifiedNodeData, T data)
+        {
+            Node<T> node = getNode(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                Node<T> searchedNode = head;
+
+                while (!searchedNode.Data.Equals(specifiedNodeData))
+                {
+                    searchedNode = searchedNode.next;
+                }
+                Node<T> tempNode = searchedNode.next;
+                searchedNode.next = node;
+                searchedNode.next.next = tempNode;
+                Console.WriteLine("{0} inserted into link list", node.Data);
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null)
+                Console.WriteLine("List is empty");
+            Console.WriteLine("{0} removed from link list", head.Data);
+            head = head.next;
+
+        }
+
+        public void RemoveLast()
+        {
+            if (head == null && head.next == null)
+                Console.WriteLine("List is empty");
+            Node<T> newNode = head;
+
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            Console.WriteLine("{0} removed from link list", newNode.next.Data);
+            newNode.next = null;
+        }
+
+        public void search(T data)
+        {
+            while (head != null)
+            {
+                if (head.Data.Equals(data))
+                    Console.WriteLine("{0} is foundin liked list.", data);
+                head = head.next;
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            else
+            {
+                Node<T> temp = head;
+
+                Node<T> previous = null;
+
+                while (!temp.Data.Equals(data))
+                {
+                    previous = temp;
+                    temp = temp.next;
+                }
+                Console.WriteLine("{0} removed from link list", temp.Data);
+                previous.next = temp.next;
+            }
+        }
+    }
+}
+
+
+
+
+
+namespace Day_14_Problems
+{
+    public class Node<T>
+    {
+        public dynamic Data;
+
+        public Node<T> next;
+
+        public Node(T data)
+        {
+            this.Data = data;
+        }
+
+    }
+}
+
